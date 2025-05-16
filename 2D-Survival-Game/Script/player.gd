44a366f02649c89aec70f78c462dea4d6c1a3eeb
Sheikh Shan Elahi <80357143+Shan166@users.noreply.github.com>
2025-05-16 20:31:26 +0500

    Commit 11 Arrow firing done

--- 2D-Survival-Game/Script/player.gd
+++ 2D-Survival-Game/Script/player.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 var speed=100
 var player_state
 @export var inv: Inv
+var bow_equiped=false
+var bow_cooldown=true
+var arrow=preload("res://Scene/arrow.tscn")
 
 func _physics_process(delta):
 	var direction=Input.get_vector("ui_left","ui_right","ui_up","ui_down")
@@ -9,9 +12,25 @@ func _physics_process(delta):
 		player_state="Idle"
 	elif direction.x !=0 or direction.y !=0:
 		player_state="Walking"
-	
 	velocity=direction*speed
 	move_and_slide()
+	if Input.is_action_just_pressed("e"):
+		if bow_equiped:
+			bow_equiped=false
+		else:
+			bow_equiped=true
+	
+	var mouse_pos=get_global_mouse_position()
+	$Marker2D.look_at(mouse_pos)
+	
+	if Input.is_action_just_pressed("left_mouse") and bow_equiped and bow_cooldown:
+		bow_cooldown=false
+		var arrow_instance=arrow.instantiate()
+		arrow_instance.rotation=$Marker2D.rotation
+		arrow_instance.global_position=$Marker2D.global_position
+		add_child(arrow_instance)
+		await get_tree().create_timer(0.4).timeout
+		bow_cooldown=true
 
 	play_anim(direction)
 	
