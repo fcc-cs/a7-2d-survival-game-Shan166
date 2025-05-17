624cbee588747ed3f723f04be90d6b14ab43e81c
Sheikh Shan Elahi <80357143+Shan166@users.noreply.github.com>
2025-05-17 23:10:06 +0500

    Commit 18 cutscene without smoke effects

--- 2D-Survival-Game/Scene/world_2.gd
+++ 2D-Survival-Game/Scene/world_2.gd
@@ -1,9 +1,30 @@
 extends Node2D
 @onready var animplayer=$world2openingcutscene/AnimationPlayer
+@onready var camera=$world2openingcutscene/Path2D/PathFollow2D/Camera2D
 var is_openingcutscene=false
 var has_player_entered_area=false
 var player=null
+var is_pathfollowing=false
+var smoke_has_happened=false
+var smoke_is_happening=false
 
+func _physics_process(delta):
+	if is_openingcutscene:
+		var pathfollower=$world2openingcutscene/Path2D/PathFollow2D
+		if is_pathfollowing:
+			if !smoke_is_happening:
+				pathfollower.progress_ratio+=0.004
+			if pathfollower.progress_ratio>=1:
+				cutscenending()
+			if !smoke_has_happened and pathfollower.progress_ratio>=0.99 and !smoke_is_happening:
+				smoke_is_happening=true
+				await get_tree().create_timer(1).timeout
+				$world2openingcutscene/TileMapfinished.visible=true
+				$world2openingcutscene/TileMapunfinished2.visible=false
+				await get_tree().create_timer(0.5).timeout
+				smoke_has_happened=true
+				smoke_is_happening=false
+				
 func _on_player_detection_body_entered(body):
 	if body.has_method("player"):
 		player=body
@@ -15,4 +36,13 @@ func cutsceneopening():
 	is_openingcutscene=true
 	animplayer.play("cover_fade")
 	player.camera.enabled=false
-	
+	camera.enabled=true
+	is_pathfollowing=true
+
+func cutscenending():
+	is_pathfollowing=false
+	is_openingcutscene=false
+	camera.enabled=false
+	player.camera.enabled=true
+	$world2openingcutscene.visible=false
+	$world2main.visible=true
